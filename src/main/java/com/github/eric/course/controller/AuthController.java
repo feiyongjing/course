bb1496e72a254606f9f385553e7d3680ad4d5d45
feiyongjing <2059326740@qq.com>
2021-08-03 22:49:23 +0800

    添加登录与鉴权的doc文档

--- src/main/java/com/github/eric/course/controller/AuthController.java
+++ src/main/java/com/github/eric/course/controller/AuthController.java
@@ -24,7 +24,7 @@ import static com.github.eric.course.configuration.UserInterceptor.COOKIE_NAME;
 
 @RestController
 @RequestMapping("/api/v1")
-public class AuthCintroller {
+public class AuthController {
     @Autowired
     private SessionDao sessionDao;
     @Autowired
@@ -34,11 +34,45 @@ public class AuthCintroller {
     @Autowired
     private UserDao userDao;
 
+    /**
+     * 管理员查看所有的用户
+     * @return 所有的用户
+     */
     @GetMapping("/admin/users")
     public List<User> getAllUsers() {
         return userRoleManagerService.getAllUsers();
     }
 
+    /**
+     * @api {get} /api/v1/session 检查登录状态
+     * @apiName 检查登录状态
+     * @apiGroup 登录与鉴权
+     *
+     * @apiHeader {String} Accept application/json
+     *
+     * @apiParamExample Request-Example:
+     *            GET /api/v1/auth
+     *
+     * @apiSuccess {User} user 用户信息
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "user": {
+     *           "id": 123,
+     *           "username": "Alice"
+     *       }
+     *     }
+     * @apiError 401 Unauthorized 若用户未登录
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "message": "Unauthorized"
+     *     }
+     */
+    /**
+     * @return 已登录的用户
+     */
     @GetMapping("/session")
     public Session authStatus() {
         User currentUser = UserContext.getCurrentUser();
@@ -51,6 +85,41 @@ public class AuthCintroller {
 
     }
 
+    /**
+     * @api {post} /api/v1/session 登录
+     * @apiName 登录
+     * @apiGroup 登录与鉴权
+     *
+     * @apiHeader {String} Accept application/json
+     * @apiHeader {String} Content-Type application/x-www-form-urlencoded
+     *
+     * @apiParam {String} username 用户名
+     * @apiParam {String} password 密码
+     * @apiParamExample Request-Example:
+     *          username: Alice
+     *          password: MySecretPassword
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 201 Created
+     *     {
+     *       "user": {
+     *           "id": 123,
+     *           "username": "Alice"
+     *       }
+     *     }
+     *
+     * @apiError 400 Bad Request 若用户的请求包含错误
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 400 Bad Request
+     *     {
+     *       "message": "Bad Request"
+     *     }
+     */
+    /**
+     * @param username 用户名
+     * @param password 密码
+     */
     @PostMapping("/session")
     public User login(@RequestParam String username, @RequestParam String password, HttpServletResponse response) {
         checkParam(username, password);
@@ -77,6 +146,29 @@ public class AuthCintroller {
 
     }
 
+    /**
+     * @api {delete} /api/v1/session 登出
+     * @apiName 登出
+     * @apiGroup 登录与鉴权
+     *
+     * @apiHeader {String} Accept application/json
+     *
+     * @apiParamExample Request-Example:
+     *            DELETE /api/v1/session
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 204 No Content
+     * @apiError 401 Unauthorized 若用户未登录
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "message": "Unauthorized"
+     *     }
+     */
+    /**
+     * @param response Http response
+     */
     @DeleteMapping("/session")
     @Transactional
     public void logout(HttpServletRequest request, HttpServletResponse response) {
@@ -91,6 +183,44 @@ public class AuthCintroller {
         response.setStatus(204);
     }
 
+    /**
+     * @api {post} /api/v1/register 用户注册
+     * @apiName 用户注册
+     * @apiGroup 登录与鉴权
+     *
+     * @apiParam {String} username  用户名
+     * @apiParam {String} password  密码
+     *
+     * @apiSuccess {Integer} id 用户id
+     * @apiSuccess {string} username 用户名
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 201 Created
+     *     {
+     *       "id": "123",
+     *       "username": "Alice"
+     *     }
+     *
+     * @apiError 400 Bad Request 若用户的请求包含错误
+     *
+     * @apiErrorExample Error-Response:
+     *      HTTP/1.1 400 Bad Request
+     *    {
+     *      "message": "Bad Request"
+     *    }
+     *
+     * @apiError 409 Conflict 若用户名已经被注册
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 409 Conflict
+     *     {
+     *       "message": "用户名已经被注册"
+     *     }
+     */
+    /**
+     * @param username 用户名
+     * @param password 密码
+     */
     @PostMapping("/user")
     public User register(@RequestParam String username, @RequestParam String password, HttpServletResponse response) {
         checkParam(username, password);
